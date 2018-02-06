e6564f991f56e25241c702c64ab5aa91a944664d
zaiym <yz10086>
2018-02-06 10:37:57 +0800

    测试修改

--- src/main/java/com/pde/DiskmonitorApplication.java
+++ src/main/java/com/pde/DiskmonitorApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 /**
- * 启动类
+ * 启动类11
  */
 @SpringBootApplication
 public class DiskmonitorApplication {
